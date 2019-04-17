9e8692024dead18f16806b6eb88199f230b116ce
lintao <917563379@qq.com>
2019-04-17 23:21:48 +0800

    modify the linear code

--- Linear_Regression/linear.py
+++ Linear_Regression/linear.py
@@ -3,6 +3,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def model(x, w, b):
+    return w * x + b
+
+def loss_function(x, y, w, b):
+    return np.sum(np.square(y - model(x, w, b))) / len(x)
+
+def optimizer(x, y, w, b, lr, iters):
+    b_gradient = 0
+    w_gradient = 0
+    N = float(len(x))
+
+    for i in range(iters):
+        pass
+
+
+
+
+
+
+def compute_gradient(data,w, b, lr):
+    w_gradient = 0
+    b_gradient = 0
+
+    N = float(len(data[0]))
+    x = data[0]
+    y = data[1]
+
+
+
 
 
 
@@ -16,13 +45,16 @@ def Linear_regression():
     # defint the number that will iteration
     # define  y =wx+b
     learning_rate = 0.001
-    initial_b = 0.0
-    initial_w = 0.0
+    b = 0.0
+    w = 1
     num_iter = 1000
-    plt.scatter(x, y)
-    plt.show()
+    # plt.scatter(x, y)
+    # plt.show()
+    loss_function(x,y,w,b)
     # print 'initial variables:\n initial_b = {0}\n intial_m = {1}\n error of begin = {2} \n' \
     #     .format(initial_b, initial_m, compute_error(initial_b, initial_m, data))
+
+
 
 
 if __name__ == '__main__':
