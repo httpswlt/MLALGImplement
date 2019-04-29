5a713bc5cdd7007ad890d7472751ccf3075d5eb9
lintao <917563379@qq.com>
2019-04-29 21:31:10 +0800

    knn refined

--- KNN/knn.py
+++ KNN/knn.py
@@ -1,9 +1,6 @@
 # coding:utf-8
 import numpy as np
 from sklearn.datasets import load_iris
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 import random
 
 
@@ -19,9 +16,8 @@ class KNN(object):
 
     def pred(self, x):
         y_pred = np.zeros((x.shape[0], 1), dtype=self.y_train.dtype)
-
         for i, x_test in enumerate(x):
-            distance = self.l1_distance(self.x_train, x_test)
+            distance = self.dist_func(self.x_train, x_test)
             nn_index = np.argsort(distance)
             nn_pred = self.y_train[nn_index][:self.n_neighbors].ravel()
             y_pred[i] = np.argmax(np.bincount(nn_pred))
@@ -51,16 +47,23 @@ def train_test_split(x, y, test_ratio):
     train_data = x[train_index]
     train_label = y[train_index]
     test_data = x[test_index]
-    test_label = x[test_index]
+    test_label = y[test_index]
     return train_data, train_label, test_data, test_label
+
+
+def accuracy_score(pred, label):
+    temp = (pred - label).ravel().tolist()
+    correct_nums = temp.count(0)
+    return correct_nums / float(pred.shape[0])
 
 
 def main():
     x, y = load_data()
     train_data, train_label, test_data, test_label = train_test_split(x, y, 0.3)
-    knn = KNN(train_data, train_label, 3)
+    knn = KNN(train_data, train_label, 9, 'l2')
     pred = knn.pred(test_data)
-    pass
+    print(accuracy_score(pred, test_label))
+
 
 if __name__ == '__main__':
     main()
