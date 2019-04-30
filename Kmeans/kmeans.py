d660e049d2ea8623378572c5be50c211bbcc50a8
lintaowx <lintaox.wu@intel.com>
2019-04-30 20:34:12 +0800

    fix the init centroid function

--- Kmeans/kmeans.py
+++ Kmeans/kmeans.py
@@ -3,7 +3,6 @@ from sklearn.datasets.samples_generator import make_blobs
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-from scipy.spatial.distance import cdist
 
 
 def load_data():
@@ -16,14 +15,18 @@ class KMeans(object):
         super(KMeans, self).__init__()
         self.n_clusters = n_clusters
         self.max_iter = max_iter
-        self.centroids = None
         self.x = x
         self.col_min = x.min(axis=0)
         self.col_max = x.max(axis=0)
         # generate random centroids.
         self.random_pool = range(0, self.x.shape[0])
-        self.centroids = np.array([self._random_centroids() for _ in range(self.n_clusters)])
-        # self.centroids = x[:6]
+        self.__centroids = []
+        for _ in range(self.n_clusters):
+            self.__centroids.append(self._random_centroids())
+
+    @property
+    def centroids(self):
+        return np.array(self.__centroids)
 
     def fit(self):
         # get the best cluster point by iteration.
@@ -31,29 +34,49 @@ class KMeans(object):
             clusters_indx = self.pred(self.x)
             for c in range(self.n_clusters):
                 if c in clusters_indx:
-                    self.centroids[c] = np.mean(self.x[c == clusters_indx], axis=0)
+                    self.__centroids[c] = np.mean(self.x[c == clusters_indx], axis=0)
                 else:
-                    self.centroids[c] = self._random_centroids()
+                    self.__centroids[c] = self._random_centroids()
 
     def _random_centroids(self):
-        return self.x[random.choice(self.random_pool)]
+        # first solution: random choose
+        # random_point = self.x[random.choice(self.random_pool)]
+
+        # Second solution: the point by selected need to have a long distance.
+        if not len(self.__centroids):
+            random_point = self.x[random.choice(self.random_pool)]
+        else:
+            distances = self.distance(self.x)
+            if distances.shape[1] == 1:
+                random_point = self.x[np.argmax(np.sum(distances, axis=1, keepdims=True))]
+            else:
+                random_point = self.x[self.calculate_best_point(distances)]
+        return random_point
 
     def pred(self, x):
         distances = self.distance(x)
-        # distances = cdist(x, self.centroids)
-        index = np.argmin(distances, axis=1)
-        return index
-        # return np.argmin(distances, axis=1)
+        return np.argmin(distances, axis=1)
 
     def distance(self, x):
         result = None
-        for centroid in self.centroids:
+        for centroid in self.__centroids:
             temp = self.dis_l1(x, centroid)
             if isinstance(result, np.ndarray):
                 result = np.hstack((result, temp))
             else:
                 result = temp
         return result
+
+    @staticmethod
+    def calculate_best_point(x):
+        min_ratio = float('inf')
+        indx = 0
+        for i, dis in enumerate(x):
+            ratio = np.max(dis) / float(np.min(dis))
+            if ratio < min_ratio:
+                min_ratio = ratio
+                indx = i
+        return indx
 
     @staticmethod
     def dis_l2(a, b):
@@ -67,7 +90,6 @@ class KMeans(object):
 def plot(x, y, centroids, title='result'):
     plt.figure(figsize=(6, 6))
     plt.scatter(x[:, 0], x[:, 1], c=y)
-    # plt.scatter(centroids[:, 0], centroids[:, 1], s=150, c=np.array(range(0, 6)))
     plt.scatter(centroids[:, 0], centroids[:, 1], s=150, c='red')
     plt.title(title)
     # plt.show()
